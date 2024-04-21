dd2755e398535301a8fcad4df9e38e3876becb19
lzrs <lzrs@modular.cloud>
2024-04-20 19:54:30 -0700

    Remove completed experiment

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createWallet, getAddress } from "./auth";
-// import { Sequencer } from "./sequencer";
+import { Sequencer } from "./sequencer";
 
 export async function middleware(request: NextRequest) {
   const readRequest = request.method === "GET" || request.method === "HEAD";
@@ -41,39 +41,31 @@ export async function middleware(request: NextRequest) {
 
   const body = await request.text();
 
-  // await Sequencer.enqueue({
-  //   method: request.method,
-  //   path: request.nextUrl.pathname,
-  //   headers: {
-  //     "accept": request.headers.get("accept") || "*/*",
-  //     "content-type": request.headers.get("content-type"),
-  //     "next-action": request.headers.get("next-action"),
-  //     "next-router-state-tree": request.headers.get("next-router-state-tree"),
-  //   },
-  //   body,
-  //   address,
-  // });
+  await Sequencer.enqueue({
+    method: request.method,
+    path: request.nextUrl.pathname,
+    headers: {
+      accept: request.headers.get("accept") || "*/*",
+      "content-type": request.headers.get("content-type"),
+      "next-action": request.headers.get("next-action"),
+      "next-router-state-tree": request.headers.get("next-router-state-tree"),
+    },
+    body,
+    address,
+  });
 
-  console.log("next-action", request.headers.get("next-action"));
+  const response = await fetch(request.url, {
+    method: request.method,
+    headers: {
+      cookie: `session=${session};`,
+      accept: request.headers.get("accept") || "*/*",
+      "content-type": request.headers.get("content-type"),
+      "next-action": request.headers.get("next-action"),
+      "next-router-state-tree": request.headers.get("next-router-state-tree"),
+    } as any,
+    body,
+  });
 
-  const response = await fetch(
-    request.url.replace(
-      String(process.env.BASE_URL),
-      "https://play.modular.cloud"
-    ),
-    {
-      method: request.method,
-      headers: {
-        cookie: `session=${session};`,
-        accept: request.headers.get("accept") || "*/*",
-        "content-type": request.headers.get("content-type"),
-        "next-action": request.headers.get("next-action"),
-        "next-router-state-tree": request.headers.get("next-router-state-tree"),
-      } as any,
-      body,
-    }
-  );
-
-  // await Sequencer.next();
+  await Sequencer.next();
   return response;
 }
